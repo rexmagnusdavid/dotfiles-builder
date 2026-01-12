961a5968fb49a8f59b1a23cbe08ca608d7412f2d
David Wang <rexmagnusdavid@gmail.com>
2026-01-12 16:37:37 -0500

    feat: Add Tailwind and Prettier

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+const config = defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    }
+  },
+  eslintConfigPrettier
+]);
+
+export default config;
---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  trailingComma: 'none'
+};
+
+export default config;
