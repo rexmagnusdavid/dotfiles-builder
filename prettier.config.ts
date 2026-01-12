8bbf648d62ab315d39c3041766301574db78cee7
David Wang <rexmagnusdavid@gmail.com>
2026-01-12 16:44:49 -0500

    feat: Further setup

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,12 +1,16 @@
-import { type Config } from 'prettier';
+import { type Config } from "prettier";
 
 const config: Config = {
   tabWidth: 2,
   useTabs: false,
   semi: true,
-  singleQuote: true,
+  singleQuote: false,
   printWidth: 80,
-  trailingComma: 'none'
+  trailingComma: "none",
+  plugins: [
+    "prettier-plugin-tailwindcss",
+    "@trivago/prettier-plugin-sort-imports"
+  ]
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const config = defineConfig({
   plugins: [react(), tailwindcss()]
